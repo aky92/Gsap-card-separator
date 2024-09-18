9e3172330fc838446cf266b00ca15cd18bc0131a
aky.tari92 <aky.tari92@gmail.com>
2024-09-18 15:29:27 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let left=document.querySelector('.left');
+let right=document.querySelector('.right');
+
+gsap.to(left,{
+    x:'-50%',
+    rotate:'-5deg',
+    duration:'7',
+    ease: "expoScale(0.5,7,none)",
+    scrollTrigger:{
+        trigger:left,
+        scroller:'body',
+        end:'top 20%',
+        start:'top 70%',
+        scrub:true,
+        //markers:true
+    }
+})
+
+gsap.to(right,{
+    x:'50%',
+    rotate:'5deg',
+    duration:'7',
+    ease: "expoScale(0.5,7,none)",
+    scrollTrigger:{
+        trigger:left,
+        scroller:'body',
+        end:'top 20%',
+        start:'top 70%',
+        scrub:true,
+        //markers:true
+    }
+})
+
