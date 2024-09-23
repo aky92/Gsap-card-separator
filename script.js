c8196ef16a0865434efe502f1487e1b3f8b6db32
aky.tari92 <aky.tari92@gmail.com>
2024-09-23 15:41:14 +0000

    sectpon one completed

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 let left=document.querySelector('.left');
 let right=document.querySelector('.right');
 
+
+let img1=document.querySelector('.dec-1__img1');
+
+
+//by using function
+let move=(target)=>{
+    gsap.from(target,{
+        opacity:0,
+        scale:'0',
+        duration:2.5,
+        ease: "expo.out",
+    })
+}
+
+move('.dec-1__img1');
+move('.dec-1__img2');
+move('.dec-1__img3');
+move('.dec-1__img4');
+
+
+
+
 gsap.to(left,{
-    x:'-80%',
+    x:'-99%',
     rotate:'-5deg',
     duration:'7',
     ease: "expoScale(0.5,7,none)",
@@ -18,7 +40,7 @@ gsap.to(left,{
 })
 
 gsap.to(right,{
-    x:'80%',
+    x:'98%',
     rotate:'5deg',
     duration:'7',
     ease: "expoScale(0.5,7,none)",
