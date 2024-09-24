576787543b6cbb4550885815bbfc5dfdc4985857
aky.tari92 <aky.tari92@gmail.com>
2024-09-24 16:03:56 +0000

    3rd section added

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let right=document.querySelector('.right');
 
 
 let img1=document.querySelector('.dec-1__img1');
-
-
+let sec__archPointer=document.querySelector('.sec__arch--pointer');
+let sec__archCenter=document.querySelector('.sec__arch--center');
 //by using function
 let move=(target)=>{
     gsap.from(target,{
@@ -14,14 +14,10 @@ let move=(target)=>{
         ease: "expo.out",
     })
 }
-
 move('.dec-1__img1');
 move('.dec-1__img2');
 move('.dec-1__img3');
 move('.dec-1__img4');
-
-
-
 
 gsap.to(left,{
     x:'-99%',
@@ -54,3 +50,34 @@ gsap.to(right,{
     }
 })
 
+document.addEventListener('mousemove',(e)=>{
+    //console.log(e);
+    gsap.to(sec__archPointer,{
+        x:e.x,
+      
+        y:e.y,
+        
+        ease: "power4.out"
+
+    })
+})
+
+
+gsap.from('.sec__arch h4',{
+    opacity:0,
+    delay:0.5,
+    scrollTrigger:{
+        trigger:'.sec__arch h4',
+        scroller:'body',
+        start:'top 55%',
+        scrub:true,
+        markers:true
+
+    }
+})
+
+// sec__archCenter.addEventListener('mouseenter',(e)=>{
+//     sec__archPointer.style.width="100px";
+//     sec__archPointer.style.height="100px";
+//     sec__archPointer.style.backgroundColor="red";
+// })
